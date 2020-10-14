d2b34dd2f9ba39aec4d2afd1820efc26c87605e2
shubhamsks <shubhamkumar7051@gmail.com>
2020-10-14 15:07:19 +0530

    s3 bucket for static storage

--- tool_rent/settings.py
+++ tool_rent/settings.py
@@ -54,6 +54,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # for static files
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -135,7 +137,22 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
 STATIC_URL = '/static/'
+
+AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'sk-django-static-s3'
+
+AWS_S3_CUSTOM_DOMAIN = '{}.s3.amazonaws.com'.format(AWS_STORAGE_BUCKET_NAME)
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATIC_URL = 'https://{0}/{1}/'.format(AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
--- tool_rent/urls.py
+++ tool_rent/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_header = 'ToolRent Admin'
 admin.site.index_title = 'ToolRent Administration'
@@ -8,6 +10,7 @@ admin.site.site_title = 'ToolRent'
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tools/', include('tools.urls')),
-    path('tools/', include('reviews.urls')),
-    
+    path('tools/', include('reviews.urls')),    
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

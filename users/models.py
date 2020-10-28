be73daf357b5e43a778c5bc13473610ae79f24da
shubhamsks <shubhamkumar7051@gmail.com>
2020-10-28 17:31:35 +0530

    upload path changed in profile model

--- users/models.py
+++ users/models.py
@@ -1,10 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
-from django.contrib.auth import get_user_model
 
 
-# User = get_user_model()
+def upload_profile(instance, filename):
+    """
+    Upload images in media/tools/ dir in s3 bucket
+    """
+    return 'profile_pics/{0}'.format(instance.user.username) + filename
+
 
 class User(AbstractUser):
     pass
@@ -16,7 +20,7 @@ class Profile(models.Model):
     like bio, profile image to a user account
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField('User Profile Pic', default='img/default_profile.jpg')
+    image = models.ImageField('User Profile Pic', upload_to=upload_profile, default='img/default_profile.jpg')
     bio = models.TextField("User Bio", help_text=_('Small description about user in 500 characters'), max_length=500)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
 

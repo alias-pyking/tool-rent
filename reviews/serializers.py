2d1920557dcb3962ce294e8b362e9bb1211ef3fa
shubhamsks <shubhamkumar7051@gmail.com>
2020-12-12 22:43:42 +0530

    fixed rating algorithm bug

--- reviews/serializers.py
+++ reviews/serializers.py
@@ -17,7 +17,7 @@ class ReviewSerializer(serializers.ModelSerializer):
     def create(self, validated_data, **kwargs):
         tool = kwargs['tool']
         total_users_rated = int(tool.total_users_rated) + 1
-        tool.total_stars = float(tool.total_stars + int(validated_data['stars'])) / total_users_rated
+        tool.total_stars = float(tool.total_stars + int(validated_data['stars']))
         tool.total_users_rated = total_users_rated
         tool.rating = float(tool.total_stars/total_users_rated)
         tool.save()
